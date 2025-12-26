931b34de12a48e29b4412a1703f8b1854742b0ba
MGFRV <madghost67@gmail.com>
2025-12-26 13:20:34 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -3,17 +3,32 @@ const fs = require('fs');
 const regions = ["California","Texas","Florida","New York","Illinois","Pennsylvania","Ohio","Georgia"];
 
 let data = [];
-for (let i = 1; i <= 100; i++) {
-  data.push({
-    id: i,
-    region: regions[Math.floor(Math.random() * regions.length)],
-    available: Math.random() > 0.5
-  });
+
+try {
+  data = JSON.parse(fs.readFileSync('map.json','utf8'));
+} catch {
+  for (let i = 1; i <= 100; i++) {
+    data.push({
+      id: i,
+      region: regions[Math.floor(Math.random() * regions.length)],
+      available: Math.random() > 0.5
+    });
+  }
 }
 
+let changed = false;
+
 data.forEach(d => {
-  if (Math.random() < 0.3) d.available = !d.available;
+  if (Math.random() < 0.2) {
+    d.available = !d.available;
+    changed = true;
+  }
 });
+
+if (!changed) {
+  const i = Math.floor(Math.random() * data.length);
+  data[i].available = !data[i].available;
+}
 
 fs.writeFileSync('map.json', JSON.stringify(data, null, 2));
 console.log('map.json updated');
