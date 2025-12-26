443393e87bd009c370800549bac7878da30503aa
MGFRV <madghost67@gmail.com>
2025-12-26 12:56:21 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const regions = ["California","Texas","Florida","New York","Illinois","Pennsylvania","Ohio","Georgia"];
 
@@ -11,14 +12,29 @@ for (let i = 1; i <= 100; i++) {
   });
 }
 
-function update() {
+function updateData() {
   data.forEach(d => {
     if (Math.random() < 0.3) d.available = !d.available;
   });
 
   fs.writeFileSync('map.json', JSON.stringify(data, null, 2));
-  console.log('Updated', new Date().toLocaleTimeString());
+  console.log('Updated map.json');
 }
 
-setInterval(update, 15000);
-update();
+function gitPush() {
+  exec('git add map.json && git commit -m "auto update" && git push', (err, stdout, stderr) => {
+    if (err) {
+      console.error('Git error:', err.message);
+    } else {
+      console.log('Pushed to GitHub');
+    }
+  });
+}
+
+setInterval(() => {
+  updateData();
+  gitPush();
+}, 15000);
+
+updateData();
+gitPush();
